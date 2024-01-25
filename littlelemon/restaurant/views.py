dda86244595edc507a5a94993be2646c669bc734
Jirapatch Moolsart <jirapatch_macbook@Jirapatchs-MacBook-Air.local>
2024-01-25 22:46:48 +0700

    Securing the table booking API

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -5,7 +5,7 @@ from .models import Menu, Booking
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
-
+from rest_framework.decorators import permission_classes
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
@@ -16,6 +16,7 @@ class UserViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     
 class MenuItemView(ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
@@ -24,6 +25,7 @@ class SingleMenuItemView(RetrieveUpdateAPIView, DestroyAPIView):
     serializer_class = MenuSerializer
 
 class BookingViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated] 
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
     
