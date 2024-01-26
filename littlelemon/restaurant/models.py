01c21c9944e46a39f9ad62022d2a3c1e723df045
Jirapatch Moolsart <jirapatch_macbook@Jirapatchs-MacBook-Air.local>
2024-01-26 16:56:35 +0700

    add venv folder

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -4,12 +4,12 @@ from django.db import models
 
 class Booking(models.Model):
     name = models.CharField(max_length=255)
-    no_of_guests = models.IntegerField(6)
+    no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField()
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2) 
-    inventory = models.IntegerField(5)
+    inventory = models.IntegerField()
     def __str__(self):
         return f'{self.title} : {str(self.price)}'
